49c9fcbf8c5932f430102a03ca54cc4e92e5f649
aegluke <aegluke>
2003-06-18 10:25:51 +0000

    test application for the wgtrackbar-widget

---
+++ tests/trackbartest.pas
@@ -0,0 +1,35 @@
+program trackbartest;
+
+{$mode objfpc}
+
+uses gfxbase, gfxform, wgtrackbar;
+
+type
+    TMainForm = class(TGfxForm)
+        public
+            trackbar : TwgTrackBar;
+            procedure AfterCreate; override;
+    end;
+
+procedure TMainForm.AfterCreate;
+begin
+    Height := 250;
+    Width := 200;
+    inherited AfterCreate;
+    WindowTitle := 'Trackbar-Test';
+    trackbar := TwgTrackBar.create(self);
+    trackbar.setdimensions(10,10,180,180);
+    trackbar.anchors := [anleft,anright,antop,anbottom];
+    trackbar.Position := 10;
+end;
+
+var
+    MainForm : TMainForm;
+
+begin
+    gfxOpenDisplay('');
+    MainForm := TMainForm.Create(nil);
+    MainForm.Show;
+    gfxDoMessageLoop;
+    gfxCloseDisplay;
+end.
