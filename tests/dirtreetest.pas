2e50caba2efa665a34053c0d130bff38a8e759b5
aegluke <aegluke>
2003-10-29 19:20:46 +0000

    test-program for TwgDirTree - initial release

---
+++ tests/dirtreetest.pas
@@ -0,0 +1,44 @@
+program dirtreetest;
+
+{$IFDEF FPC}
+    {$mode objfpc}
+    {$H+}
+{$ELSE}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses sysutils, unitkeys,messagequeue, schar16, gfxbase, gfxstyle, gfxform, wgdirtree;
+
+type
+    TMainForm = class(TGfxForm)
+      public
+	DirTree : TwgDirTree;
+	procedure AfterCreate; override;
+    end;
+
+var
+    MainForm : TGfxForm;
+
+procedure TMainForm.AfterCreate;
+begin
+    inherited;
+    SetDimensions(500,10,400,400);
+    WindowTitle8 := 'DirTree-Test';
+    DirTree := TwgDirTree.Create(self);
+    DirTree.top := 100;
+    DirTree.left := 10;
+    DirTree.height := 200;
+    DirTree.width := 200;
+    DirTree.anchors := [anLeft,anBottom, anRight, anTop];
+//    DirTree.ReadDirectories(DirTree.RootNode);
+end;
+
+
+begin
+    GfxOpenDisplay('');
+    MainForm := TMainForm.Create(nil);
+    MainForm.Show;
+    GfxDoMessageLoop;    
+    GfxCloseDisplay;    
+end.
+
