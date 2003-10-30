6921a0e124672cb7d0e107987ca7da30e3c61b63
Nagy Viktor <nvitya@gmail.com>
2003-10-30 02:34:14 +0000

    image library change

--- src/gfxstdimg.pas
+++ src/gfxstdimg.pas
@@ -17,16 +17,25 @@ implementation
 {$I stdimg_cancel.inc}
 
 procedure GfxCreateStandardImages;
+var
+  img : TGfxImage;
 begin
-  GfxLibAddMaskedBMP( 'STDIMG.OK',
-                      @stdimg_ok,
-                sizeof(stdimg_ok),
-                      0,0 );
+  img := GfxLibAddMaskedBMP(
+            'STDIMG.OK',
+            @stdimg_ok,
+      sizeof(stdimg_ok),
+            0,0 );
+                      
+  GfxLibAddImage( 'STDIMG.YES', img );
                 
-  GfxLibAddMaskedBMP( 'STDIMG.CANCEL',
-                      @stdimg_cancel,
-                sizeof(stdimg_cancel),
-                      0,0 );
+  img := GfxLibAddMaskedBMP(
+            'STDIMG.CANCEL',
+            @stdimg_cancel,
+      sizeof(stdimg_cancel),
+            0,0 );
+
+  GfxLibAddImage( 'STDIMG.NO', img );
+
 end;
 
 end.
