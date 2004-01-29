daa9710a0e5732e2bac5467ad7d24110f4f6eb04
aegluke <aegluke>
2004-01-29 11:07:55 +0000

    Filedialog-Test

---
+++ tests/filedialogtest.pas
@@ -0,0 +1,37 @@
+program
+    filedialogtest;
+{$IFDEF fpc}{$mode objfpc}{$ENDIF}
+
+uses
+    gfxbase, classes, gfxform, wgfiledialog, wgbutton;
+    
+type TMainForm = class(TgfxForm)
+           FFileDialog : TwgFileDialog;
+           FButton : TwgButton;
+           procedure AfterCreate; override;
+           procedure OnClick(ASender : TObject);
+     end;
+
+procedure TMainForm.OnClick(ASender : TObject);
+begin
+     FFileDialog := TwgFileDialog.Create(nil);
+     FFileDialog.WindowTitle8 := 'Open file';
+     FFileDialog.Execute;
+     FFileDialog.Free;
+end;
+
+procedure TMainForm.AfterCreate;
+begin
+     SetDimensions(0,0,100,100);
+     FButton := CreateButton(self, 10,10,80, 'Filedialog',@OnClick);
+end;
+
+var
+    MainForm : TMainForm;    
+begin
+    gfxOpenDisplay('');
+    MainForm := TMainForm.Create(nil);
+    MainForm.Show;
+    gfxDoMessageLoop;
+    gfxCloseDisplay;
+end.
