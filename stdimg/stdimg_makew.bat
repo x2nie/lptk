79119518df217f15f8b0b3045c809efa62fcfd94
Nagy Viktor <nvitya@gmail.com>
2004-04-21 13:57:00 +0000

    std images

---
+++ stdimg/stdimg_makew.bat
@@ -0,0 +1,1 @@
+bin2obj -c stdimg_%1 ../stdimg/%1.bmp > inc/%1.inc
---
+++ stdimg/stdimg_updatew.bat
@@ -0,0 +1,18 @@
+call stdimg_makew ok
+call stdimg_makew cancel
+call stdimg_makew close
+call stdimg_makew close2
+call stdimg_makew close3
+call stdimg_makew document
+call stdimg_makew exit
+call stdimg_makew folder
+call stdimg_makew foldernew
+call stdimg_makew folderopen
+call stdimg_makew folderup
+call stdimg_makew new
+call stdimg_makew no
+call stdimg_makew ok
+call stdimg_makew open
+call stdimg_makew save
+call stdimg_makew yes
+
