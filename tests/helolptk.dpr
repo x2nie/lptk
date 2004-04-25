fe2d2e802297a144d730680b6c02cd958c18207d
Nagy Viktor <nvitya@gmail.com>
2004-04-25 10:36:52 +0000

    helo example

---
+++ tests/helolptk.dpr
@@ -0,0 +1,47 @@
+program helolptk;
+
+{$ifdef FPC}
+  {$mode delphi}{$H+}
+{$else}
+  {$apptype CONSOLE}
+{$endif}
+
+uses
+  SysUtils, Classes, gfxbase, schar16, gfxform, wglabel, wgbutton;
+
+type
+  TfrmMain = class(TGfxForm)
+  public
+    btnClose : TwgButton;
+    lbLabel1 : TwgLabel;
+
+    procedure AfterCreate; override;
+
+    procedure CloseClick(sender : TObject);
+  end;
+
+procedure TfrmMain.AfterCreate;
+begin
+  SetDimensions(338,154,153,81);
+  WindowTitle8 := 'Hello LPTK ';
+
+  lbLabel1 := CreateLabel(self, 24,12, 'Hello from LPTK');
+
+  btnClose := CreateButton(self, 28, 40, 96, 'Close', CloseClick);
+  btnClose.ImageName := 'stdimg.close';
+end;
+
+procedure TfrmMain.CloseClick(sender: TObject);
+begin
+  Close;
+end;
+
+var
+  mfrm : TfrmMain;
+begin
+  gfxOpenDisplay('');
+  mfrm := TfrmMain.Create(nil);
+  mfrm.Show;
+  gfxDoMessageLoop;
+  gfxCloseDisplay;
+end.
